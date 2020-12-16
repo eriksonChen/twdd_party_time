9d18e7cd97ba6e67bbff45e4ad6753ced6bf2242
Erikson Chen <botan3951@gmail.com>
2020-12-16 20:51:27 +0800

    file update

--- assets/js/components/detail-modal.js
+++ assets/js/components/detail-modal.js
@@ -15,7 +15,7 @@ export default {
           <div class="pic-list">
             <div class="swiper-wrapper">
               <div class="swiper-slide" v-for="pic in detail.images">
-                <img :src="pic+'?text='+detail.en+detail.name">
+                <img v-bind:src="pic">
               </div>
             </div>
             <div class="swiper-pagination"></div>
@@ -31,24 +31,25 @@ export default {
           </div>
         </div>
         
-        
         <div class="store-info">
           <div class="modal-subtitle">店家介紹</div>
           <p>{{detail.intro}}</p>
           <div class="modal-subtitle" style="margin-top:20px;">店家資訊</div>
           <div class="info-list">
-            <p><strong>地址</strong>{{detail.addr}}</p>
+            <p><strong>地址</strong> 
+              <a v-bind:href="detail.map">
+                {{detail.addr}} 
+                <span class="icon"> <img src="./assets/img/svg/launch.svg"> </span> 
+              </a>
+            </p>
             <p><strong>電話</strong>{{detail.tel}}</p>
             <p><strong>營業時間</strong>{{detail.time}}</p>
           </div>
         </div>
         <!-- <div class="note"></div>  -->
         <div class="social-btns">
-          <a :href="detail.fb" target="_blank" class="btn">
+          <a v-bind:href="detail.fb" target="_blank" class="btn">
             <img src="./assets/img/svg/facebook-icon.svg">
-          </a>
-          <a :href="detail.ig" target="_blank" class="btn">
-            <img src="./assets/img/svg/ig-icon.svg">
           </a>
         </div>
         
@@ -63,7 +64,6 @@ export default {
     };
   },
   mounted: function () {
-    console.log(this.detail);
     const swiper = new Swiper('.pic-list', {
       pagination: {
         el: '.swiper-pagination',
